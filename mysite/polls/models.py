ba9934530f85a455dd853589a56acf2649d92055
Robel Berhanu <robelberhanu89@gmail.com>
2023-06-26 15:42:36 +0200

    modified models.py, added string methods for database fields.

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 
+
+
 # Model for Questions.
 class Question(models.Model):
+
+    # String method to for convenience
+    def __str__(self):
+        return self.question_text
+    
     question_text = models.CharField(max_length=200)
     pup_date = models.DateField("date published")
 
 
 class Choice(models.Model):
+
+    #String method to for convenience
+    def __str__(self):
+        return self.choice_text
+    
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
