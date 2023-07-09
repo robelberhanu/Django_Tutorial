35df4deebffc5c8afe18e4c54915ec160c7219ba
Robel Berhanu <robelberhanu89@gmail.com>
2023-07-09 13:01:36 +0200

    added a was published method.

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-
-
+from django.utils import timezone
+import datetime
 
 # Model for Questions.
 class Question(models.Model):
@@ -9,8 +9,11 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
     question_text = models.CharField(max_length=200)
-    pup_date = models.DateField("date published")
+    pub_date = models.DateField("date published")
 
 
 class Choice(models.Model):
