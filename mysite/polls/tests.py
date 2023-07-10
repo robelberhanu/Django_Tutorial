665db834c49ca4fef745878077350b5a26726665
Robel Berhanu <robelberhanu89@gmail.com>
2023-07-10 12:08:50 +0200

    preparing imports for method testing.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
