d962a98c1995d494d969a4b6b162aeeba4331736
Robel Berhanu <robelberhanu89@gmail.com>
2023-07-14 12:21:51 +0200

    fixed indentation errors.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -40,13 +40,13 @@ class QuestionModelTests(TestCase):
 
 
     def create_question(question_text, days):
-    """
-    Create a question with the given `question_text` and published the
-    given number of `days` offset to now (negative for questions published
-    in the past, positive for questions that have yet to be published).
-    """
-    time = timezone.now() + datetime.timedelta(days=days)
-    return Question.objects.create(question_text=question_text, pub_date=time)
+        """
+        Create a question with the given `question_text` and published the
+        given number of `days` offset to now (negative for questions published
+        in the past, positive for questions that have yet to be published).
+        """
+        time = timezone.now() + datetime.timedelta(days=days)
+        return Question.objects.create(question_text=question_text, pub_date=time)
 
 
 class QuestionIndexViewTests(TestCase):
