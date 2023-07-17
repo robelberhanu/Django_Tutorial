2737bb3f5b4cfc62a510a5c751f164aecbe870e9
Robel Berhanu <robelberhanu89@gmail.com>
2023-07-17 14:59:55 +0200

    updated tests

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -94,14 +94,4 @@ class QuestionIndexViewTests(TestCase):
             [question],
         )
 
-    def test_two_past_questions(self):
-        """
-        The questions index page may display multiple questions.
-        """
-        question1 = create_question(question_text="Past question 1.", days=-30)
-        question2 = create_question(question_text="Past question 2.", days=-5)
-        response = self.client.get(reverse("polls:index"))
-        self.assertQuerySetEqual(
-            response.context["latest_question_list"],
-            [question2, question1],
-        )
+    
