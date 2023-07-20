27ab35a3c00bbf5a01d575cf653cfcdcd908ea88
Robel Berhanu <robelberhanu89@gmail.com>
2023-07-20 11:59:13 +0200

    created test_future_question_and_past_question  method so that even if both past and future questions exist, only past questions are displayed.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -80,3 +80,15 @@ class QuestionIndexViewTests(TestCase):
             response.context["latest_question_list"],
             [question],
         )
+    def test_future_question_and_past_question(self):
+        """
+        Even if both past and future questions exist, only past questions
+        are displayed.
+        """
+        question = QuestionModelTests.create_question(question_text="Past question.", days=-30)
+        QuestionModelTests.create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(
+            response.context["latest_question_list"],
+            [question],
+        )
